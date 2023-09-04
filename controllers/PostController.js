f37b7785ce18fc50f89905c95ade765db2ece345
Arthur <konodiobrandoda@mail.ru>
2023-09-04 13:55:40 +0300

    Добавил контроллер получения поста

---
+++ controllers/PostController.js
@@ -0,0 +1,66 @@
+import PostModel from "../models/Post.js"
+
+export const getAll = async (req, res) => {
+    try {
+        const posts = await PostModel.find().populate("user").exec()
+
+        res.json(posts)
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          message: "Не удалось получить статьи.",
+        });
+    }
+}
+
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id
+
+        const doc = await PostModel.findByIdAndUpdate({
+            _id: postId
+        },
+        {
+            $inc: { viewCount: 1 }
+        },
+        {
+            returnDocument: 'after'
+        })
+
+        if (!doc) {
+            return res.status(404).json({
+                message: "Статья не найдена."
+            })
+        }
+
+        res.json(doc)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          message: "Не удалось получить статью.",
+        });
+    }
+}
+
+export const create = async (req, res) => {
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.text,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.userId
+        })
+
+        const post = await doc.save()
+
+        res.json(post)
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Не удалось создать статью."
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from "express"
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
 import mongoose from "mongoose"
 
-import { registerValidation } from "./validations/auth.js"
-import { validationResult } from "express-validator"
-import UserModel from "./models/User.js"
+import { registerValidation, loginValidation, postCreateValidation } from "./validations.js"
+import checkAuth from './utils/checkAuth.js'
+import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js"
 
 mongoose.connect(
     "mongodb+srv://arthurshelbaev:arthur123@cluster0.nkb13ye.mongodb.net/blog?retryWrites=true&w=majority")
@@ -16,92 +15,14 @@ const app = express()
 
 app.use(express.json())
 
-app.post("/login", async (req, res) => {
-    try {
-        const user = await UserModel.findOne({ email: req.body.email })
+app.post("/auth/register", registerValidation, UserController.register);
+app.post("/auth/login", loginValidation, UserController.login);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
-        if (!user) {
-            return res.status(404).json({
-                message: "Неправильный логин или пароль"
-            })
-        }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-
-        if (!isValidPass) {
-            return res.status(404).json({
-                message: "Неверный пароль или логин"
-            })
-        }
-
-        const token = jwt.sign(
-            {
-                _id: user._id
-            },
-            "secret123",
-            {
-                expiresIn: "30d"
-            }
-        )
-
-        const { passwordHash, ...userData } = user._doc
-    
-        res.json({
-            ...userData,
-            token
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: "Не удалось авторизоваться."
-        })
-    }
-})
-
-app.post("/register", registerValidation, async (req, res) => {
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) res.status(400).json(errors.array())
-    
-        const password = req.body.password
-        const salt = await bcrypt.genSalt(10)
-        const hash = await bcrypt.hash(password, salt)
-    
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            passwordHash: hash,
-            avatarUrl: req.body.avatarUrl,
-        })
-    
-        const user = await doc.save()
-
-        const token = jwt.sign(
-            {
-                _id: user._id
-            },
-            "secret123",
-            {
-                expiresIn: "30d"
-            }
-        )
-
-        const { passwordHash, ...userData } = user._doc
-    
-        res.json({
-            ...userData,
-            token
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: "Не удалось зарегистрироваться."
-        })
-    }
-})
+app.get("/posts", PostController.getAll)
+app.get("/posts/:id", PostController.getOne)
+app.post("/posts", checkAuth, postCreateValidation, PostController.create)
 
 app.listen(4444, (err) => {
-    err ? console.log(err) : console.log("Server activated");;
+    err ? console.log(err) : console.log("Server activated.");;
 })
