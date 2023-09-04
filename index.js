600e9d8047e05b3d1fe716e7d3087eeb00e062e8
arthurshelbaev <konodiobrandoda@mail.ru>
2023-09-04 18:31:36 +0500

    Добавлена загрузка файлов

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import mongoose from "mongoose"
+import multer from "multer"
 
 import { registerValidation, loginValidation, postCreateValidation } from "./validations.js"
 import checkAuth from './utils/checkAuth.js'
@@ -13,17 +14,35 @@ mongoose.connect(
 
 const app = express()
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({ storage })
+
 app.use(express.json())
+app.use("/uploads", express.static("uploads"))
 
 app.post("/auth/register", registerValidation, UserController.register);
 app.post("/auth/login", loginValidation, UserController.login);
 app.get("/auth/me", checkAuth, UserController.getMe);
 
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
+
 app.get("/posts", PostController.getAll)
 app.get("/posts/:id", PostController.getOne)
 app.post("/posts", checkAuth, postCreateValidation, PostController.create)
 app.delete("/posts/:id",  PostController.remove)
-app.patch("/posts/:id", checkAuth, PostController.update)
+app.patch("/posts/:id", checkAuth, postCreateValidation, PostController.update)
 
 app.listen(4444, (err) => {
     err ? console.log(err) : console.log("Server activated.");;
