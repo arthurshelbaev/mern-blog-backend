e23780e40162e8e9fdb21f2a473a7830aec93897
Arthur <konodiobrandoda@mail.ru>
2023-09-05 20:59:38 +0300

    Рефакторинг кода

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ import mongoose from "mongoose"
 import multer from "multer"
 
 import { registerValidation, loginValidation, postCreateValidation } from "./validations.js"
-import checkAuth from './utils/checkAuth.js'
-import * as UserController from "./controllers/UserController.js";
-import * as PostController from "./controllers/PostController.js"
+import { UserController, PostController } from "./controllers/index.js"
+import { checkAuth, handleValidationErrors } from "./utils/index.js"
 
 mongoose.connect(
     "mongodb+srv://arthurshelbaev:arthur123@cluster0.nkb13ye.mongodb.net/blog?retryWrites=true&w=majority")
-    .then(() => console.log("Database connected"))
+    .then(() => console.log("Database connected."))
 .catch((err) => console.log(err))
 
 const app = express()
@@ -20,7 +19,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname)
-    },
+    }
 })
 
 const upload = multer({ storage })
@@ -28,8 +27,8 @@ const upload = multer({ storage })
 app.use(express.json())
 app.use("/uploads", express.static("uploads"))
 
-app.post("/auth/register", registerValidation, UserController.register);
-app.post("/auth/login", loginValidation, UserController.login);
+app.post("/auth/register", registerValidation, handleValidationErrors, UserController.register);
+app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
 app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
@@ -38,12 +37,11 @@ app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
     })
 })
 
-app.get("/posts", PostController.getAll)
-app.get("/posts/:id", PostController.getOne)
-app.post("/posts", checkAuth, postCreateValidation, PostController.create)
-app.delete("/posts/:id",  PostController.remove)
-app.patch("/posts/:id", checkAuth, postCreateValidation, PostController.update)
-
 app.listen(4444, (err) => {
     err ? console.log(err) : console.log("Server activated.");;
 })
+app.get("/posts", PostController.getAll)
+app.get("/posts/:id", PostController.getOne)
+app.post("/posts", checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
+app.delete("/posts/:id", PostController.remove)
+app.patch("/posts/:id", checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
